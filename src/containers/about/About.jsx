5f0ce67848597324d332ea889ebbcfdd6fa3b669
Kai Wang <kdwang0917@gmail.com>
2023-09-12 00:43:05 -0400

    update info for fall 2023

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
     <hr className='break'></hr>
     <div className='body'>
       Welcome to my personal website!<br></br><br></br>
-      I am an entry-level web designer and software developer, with a 3.73 GPA and 2+ years of experience in Python, React, and C++.<br></br><br></br>
+      I am an entry-level web designer and software developer, with a 3.7 GPA and 3+ years of experience in Python, React, and C++.<br></br><br></br>
       I have a passion for programming and problem solving, and I'm always eager to find unique solutions to challenging problems.<br></br><br></br>
       </div>
     <hr className='break'></hr>
